862207c02498bb75e8666d26aa2b06de75ddbbdb
yutianzhengchong <337111893@qq.com>
2020-03-16 15:34:02 +0800

    路由规则定义修改

--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 用按需加载的方式配置路由，不用在下边写，直接在routes外边定义变量
 // layout变成一级，home首页变成二级，以及另外三个
-const Layout = () => import('@views/layout')// 按需引入layout
-const home = () => import('@views/home')// 按需引入home，二级路由
-const question = () => import('@views/question')// 按需引入question，二级路由
-const video = () => import('@views/video')// 按需引入video，二级路由
-const user = () => import('@views/user')// 按需引入user，二级路由
+const Layout = () => import('@/views/layout')// 按需引入layout
+const home = () => import('@/views/home')// 按需引入home，二级路由
+const question = () => import('@/views/question')// 按需引入question，二级路由
+const video = () => import('@/views/video')// 按需引入video，二级路由
+const user = () => import('@/views/user')// 按需引入user，二级路由
 // 其他一级组件
-const chat = () => import('@views/user/chat')// 按需引入小智同学
-const login = () => import('@views/login')// 按需引入登录模块
-const profile = () => import('@views/user/profile')// 按需引入编辑资料组件
-const search = () => import('@views/search')// 按需引入搜索中心组件
-const searchresult = () => import('@views/search/result')// 按需引入搜索结果组建
-const article = () => import('@views/article')// 按需引入文章详情
+const chat = () => import('@/views/user/chat')// 按需引入小智同学
+const login = () => import('@/views/login')// 按需引入登录模块
+const profile = () => import('@/views/user/profile')// 按需引入编辑资料组件
+const search = () => import('@/views/search')// 按需引入搜索中心组件
+const searchresult = () => import('@/views/search/result')// 按需引入搜索结果组建
+const article = () => import('@/views/article')// 按需引入文章详情
 
 const routes = [
   {
