cc8ce07274d895762c112c62c1b62a35c5bd0417
yutianzhengchong <337111893@qq.com>
2020-03-16 16:58:09 +0800

    完成Layout组件

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible'
+import '@/styles/index.less'// 引入全局的自定义样式  因为要覆盖vant的样式
+
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 
 Vue.config.productionTip = false
