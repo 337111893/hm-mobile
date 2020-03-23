ad1f57ca2bc39aa0d592b2a4b4ab068b6c2cd038
yutianzhengchong <337111893@qq.com>
2020-03-23 12:34:00 +0800

    不感兴趣接口eventbus删除成功

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 整体必须有container -->
   <div class="container">
-    <van-tabs >
+    <van-tabs v-model="activeIndex">
           <!-- 内部需要放置子 标签  title值为当前显示的内容-->
          <!-- van-tab是vant组件的样式  -->
        <!-- 拿到channels数据之后吧 应该做什么 -->
@@ -26,7 +26,7 @@
     <!-- 弹层组件 -->
     <van-popup :style="{ width: '80%' }" v-model="showMoreAction">
       <!-- 放置反馈组件 -->
-      <more-action ></more-action>
+      <more-action @dislike="dislikeArticle"></more-action>
     </van-popup>
   </div>
 </template>
@@ -35,6 +35,8 @@
 import ArticleList from './components/article-list'
 import MoreAction from './components/more-action'
 import { getMyChannels } from '@/api/channels'
+import { dislikeArticle } from '@/api/articles' // 不感兴趣
+import eventbus from '@/utils/eventbus' // 公共事件处理器
 
 export default {
   components: {
@@ -44,7 +46,8 @@ export default {
     return {
       channels: [], // 接收频道数据
       showMoreAction: false, // 控制反馈组件显示隐藏
-      articleId: null // 定义一个值接收,点击文章的id
+      articleId: null, // 定义一个值接收,点击文章的id
+      activeIndex: 0 // 当前默认激活的页面0
     }
   },
   methods: {
@@ -58,6 +61,31 @@ export default {
       this.showMoreAction = true
       //  应该把id给存储起来
       this.articleId = artId
+    },
+    // 对文章不感兴趣
+    async dislikeArticle () {
+      // 调用不感兴趣的文章接口
+      try {
+        await dislikeArticle({
+          target: this.articleId // 不感兴趣的id
+        })
+        // await下方的逻辑 是 resolve(成功)之后 的
+        this.$ynotify({
+          type: 'success',
+          message: '操作成功'
+        })
+        // 应该 触发一个事件 利用事件广播的机制 通知对应的tab 来删除 文章数据
+        // 除了 传一个文章之外 你还需要告诉 监听事件的人 现在处于哪个频道 可以传递频道id
+        // this.channels[this.activeIndex].id // 当前激活的频道数据
+        eventbus.$emit('delArticle', this.articleId, this.channels[this.activeIndex].id)
+        // 监听了这个事件组件 就要根据id来删除数据
+        this.showMoreAction = false // 此时关闭弹层
+      } catch (error) {
+        // 默认是红色
+        this.$ynotify({
+          message: '操作失败'
+        })
+      }
     }
   },
   created () {
