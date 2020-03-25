0de19089289fefb5074d60854765e37159ae57b9
yutianzhengchong <337111893@qq.com>
2020-03-25 17:36:26 +0800

    文章详情-关注与取消关注

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -9,7 +9,7 @@
           <p class="name">{{ article.aut_name }}</p>
           <p class="time">{{ article.pubdate | relTime }}</p>
         </div>
-        <van-button round size="small" type="info">{{ article.is_followed ? '已关注' : '+ 关注'  }}</van-button>
+        <van-button :loading='followLoading' @click="follow()" round size="small" type="info">{{ article.is_followed ? '已关注' : '+ 关注'  }}</van-button>
       </div>
       <div class="content" v-html="article.content">
         <!-- 文章内容 有标签 有属性 有样式 将标签设置到对应的元素中 v-html -->
@@ -21,22 +21,64 @@
         <van-button round size="small" :class="{active: article.attitude === 0}" plain icon="delete">不喜欢</van-button>
       </div>
     </div>
+        <!-- 放置一个遮罩层 -->
+    <van-overlay :show="loading" >
+      <!-- 加载进度条 -->
+      <div class='loading-container'>
+         <van-loading />
+      </div>
+    </van-overlay>
   </div>
 </template>
 
 <script>
 import { getArticleInfo } from '@/api/articles'
+import { followUser, unFollowUser } from '@/api/user'
 export default {
   data () {
     return {
-      article: {} // 接收文章数据
+      article: {}, // 接收文章数据
+      followLoading: false, // 是否正在点关注
+      loading: false// 遮罩层状态
     }
   },
   methods: {
     // 获取文章详情
     async getArticleInfo () {
+      this.loading = true // 打开遮罩弹层
       const { artId } = this.$route.query // 从路由信息中读取query参数
       this.article = await getArticleInfo(artId) // 查询数据
+      this.loading = false // 关闭遮罩弹层
+    },
+    // 关注或者取消关注
+    async follow () {
+      this.followLoading = true
+      try {
+      //  调用 关注或者取消关注
+      // 关注  => 取消关注
+      // 未关注 => 关注
+        if (this.article.is_followed) {
+        // 取消关注
+          await unFollowUser(this.article.aut_id) // 传入作者id
+        } else {
+        // 关注
+          await followUser({ target: this.article.aut_id }) // 传入作者id
+        }
+        // 如果成功了
+        // pc端 是 重新加载
+        // 移动端 不会重新加载 只会修改对应的数据状态
+        this.article.is_followed = !this.article.is_followed // 将状态改为对立状态
+        this.$ynotify({ type: 'success', message: '操作成功' })
+      } catch (error) {
+        // 失败会进入到catch
+        // error.message 是报错信息 就可以知道哪里出问题 这个错误是从哪里抛出来的
+        //  现在写的是中间代码   => 编译的  =>  报错指示的是编译之后的代码 => 推导出哪里出问题了
+
+        this.$ynotify({ message: '操作失败' })
+      } finally {
+        // 无论你写的执行时成功还是失败，无论走try还是catch最终都会进入finally
+        this.followLoading = false// 关闭加载状态
+      }
     }
   },
   created () {
@@ -51,6 +93,16 @@ export default {
   height: 100%;
   overflow-y: auto;
   box-sizing: border-box;
+}
+.loading-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.van-overlay {
+  background: none;
 }
 .detail {
   padding: 46px 10px 44px;
